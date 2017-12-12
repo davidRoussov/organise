273bb84b40bdfbb41d5090b808f09a4faca7511f
David Roussov <david.roussov@hotmail.com>
2017-12-12 20:31:22 +1100

    get data and create new category F2 actions

--- client/src/actions/formatTwo.js
+++ client/src/actions/formatTwo.js
@@ -1,3 +1,45 @@
 import { SERVER_URL } from '../config';
 import handleErrors from './utilities';
 
+export const getCategoriesAndNotes = () => dispatch => {
+  dispatch({ type: 'LOADING' });
+
+  fetch(`${SERVER_URL}/api/f2`, {
+    credentials: 'include'
+  })
+  .then(handleErrors)
+  .then(response => response.json())
+  .then(response => {
+    console.log('SUCCESS!!');
+    console.log(JSON.stringify(response, null, 2));
+  })
+  .catch(error => {
+    console.log("ERROR!");
+    console.log(JSON.stringify(error, null, 2));
+  })
+  .then(() => dispatch({ type: 'DONE_LOADING' }));
+};
+
+export const createNewCategory = newCategory => dispatch => {
+  dispatch({ type: 'LOADING_NEW_CATEGORY' });
+
+  fetch(`${SERVER_URL}/api/f2/category`, {
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({ newCategory }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(handleErrors)
+  .then(response => response.json())
+  .then(repsonse => {
+    console.log('SUCCESS');
+    console.log(JSON.stringify(response, null, 2));
+  })
+  .catch(error => {
+    console.log('ERROR!');
+    console.log(JSON.stringify(error, null, 2));
+  })
+  .then(() => dispatch({ type: 'DONE_LOADING_NEW_CATEGORY' }));
+}
