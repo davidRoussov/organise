01992ee9f732a5c9b4aae84cc78d4775a767ac7c
David Roussov <david.roussov@hotmail.com>
2017-12-13 22:48:25 +1100

    ignore vim

--- secret.js
+++
@@ -1,5 +0,0 @@
-export const MONGO_URI = process.env.NODE_ENV === 'production' ?
-  `mongodb://david.roussov:hhuhb876g0@ds044667.mlab.com:44667/organise` :
-  'mongodb://localhost/organise';
-
-export const SESSION_SECRET = '8t498thuf489afur389wrnf;ve9faw0e3v9[3v39-wanrwaeirufhw37nva';
