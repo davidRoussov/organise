c8f2882707218230a73cba3cb0fa21ff22623dc6
David Roussov <david.roussov@hotmail.com>
2017-12-16 21:07:33 +1100

    F2 note heading is an input

--- client/src/components/F2/F2Note.js
+++ client/src/components/F2/F2Note.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class F2Note extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      noteHeading: ''
+    };
+  }
+
+  componentWillReceiveProps(props) {
+    if(props.heading) {
+      this.setState({ noteHeading: props.heading });
+    }
+  }
+
+  handleChangeNoteHeading = e => this.setState({ noteHeading: e.target.value });
+
   render() {
 
     const list = this.props.data.items && this.props.data.items.map(item => 
@@ -10,7 +26,14 @@ class F2Note extends Component {
 
     return (
       <div className="card bg-light mb-3" style={{maxWidth: "20rem"}}>
-        <div className="card-header">{ this.props.data.heading }</div>
+        <div className="card-header">
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.noteHeading}
+            onChange={this.handleChangeNoteHeading.bind(this)}
+          />   
+        </div>
         <div className="card-body">
           {list}
         </div>
