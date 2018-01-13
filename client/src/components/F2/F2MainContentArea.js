767d3598c0026e73cfa2f178d0e16feb0771f6da
David Roussov <david.roussov@hotmail.com>
2018-01-13 19:08:27 +1100

    F2MainContentArea does not rerender after the first; this fixes bug where focus is lost after loading

--- client/src/components/F2/F2MainContentArea.js
+++ client/src/components/F2/F2MainContentArea.js
@@ -9,9 +9,12 @@ class F2MainContentArea extends Component {
   constructor() {
     super();
     this.state = {
-      filteredNotes: []
+      filteredNotes: [],
+      shouldComponentUpdate: true
     };
   }
+
+  shouldComponentUpdate = () => this.state.shouldComponentUpdate
 
   componentDidMount() {
     this.props.getNotes();
@@ -21,7 +24,9 @@ class F2MainContentArea extends Component {
     if(props.notes) {
       const notesInCategory = props.notes.filter(note => note.categoryID === props.currentCategory);
       this.setState({ filteredNotes: [] }, () => {
-        this.setState({ filteredNotes: notesInCategory }); 
+        this.setState({ filteredNotes: notesInCategory }, () => {
+          this.setState({ shouldComponentUpdate: false }); 
+        }); 
       });
     }
   }
